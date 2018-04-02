0c23e1878ae2fad64055e45603ad8789b1597b17
gbe <geoffrey.berard@lesfurets.com>
2018-04-02 18:59:32 +0200

    changing block styles

--- src/component/container/BlockContainer.js
+++ src/component/container/BlockContainer.js
@@ -5,7 +5,9 @@ import "../../styles/block-container.less"
 const BlockContainer = props => (
     <div className="block-container">
         <div className="block-label">{props.block.label}</div>
-        {props.children}
+        <div className="block-inner">
+            {props.children}
+        </div>
     </div>
 );
 
