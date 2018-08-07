6692938dbb6e8d9948df410fd136a5708968531e
gbe <geoffrey.berard@lesfurets.com>
2018-08-07 17:34:26 +0200

    FormEditor    * Allowing block expand/collapse

--- example/app/view/BlockEditorView.js
+++ example/app/view/BlockEditorView.js
@@ -5,7 +5,11 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import IconButton from "@material-ui/core/IconButton";
 import Clear from "@material-ui/icons/Clear";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 import {LabelEditor} from "../elements/LabelEditor";
+
+import Collapse from "@material-ui/core/es/Collapse/Collapse";
+import CardContent from "@material-ui/core/es/CardContent/CardContent";
 
 import "../../styles/view/block-editor-view.less"
 
@@ -19,21 +23,48 @@ function drag(ev) {
     console.log("drag",ev.target.id);
 }
 
-export const BlockEditorView = ({block, onEvent}) => (
-    <Card raised
-          id={block.id}
-          className="BlockEditorView"
-          draggable="true"
-          onDragStart={drag}>
-        <CardHeader title={<LabelEditor label={block.label}
-                                        onChange={(value => onEvent(BLOCK_EDITOR_EVENT.EDIT_LABEL, value))}/>}
-                    action={
-                        <IconButton onClick={() => onEvent(BLOCK_EDITOR_EVENT.REMOVE)}>
-                            <Clear color="primary"/>
-                        </IconButton>
-                    }/>
-    </Card>
-);
+export class BlockEditorView extends React.Component {
+    constructor(){
+        super();
+        this.state = {expanded: false};
+        this.handleExpandClick = this.handleExpandClick.bind(this);
+    }
+
+    handleExpandClick() {
+        this.setState({ expanded: !this.state.expanded });
+    };
+
+    render() {
+        let {block, onEvent} = this.props;
+        return (
+            <Card raised
+                  id={block.id}
+                  className="BlockEditorView"
+                  draggable="true"
+                  onDragStart={drag}>
+                <CardHeader title={<LabelEditor label={block.label}
+                                                onChange={(value => onEvent(BLOCK_EDITOR_EVENT.EDIT_LABEL, value))}/>}
+                            action={[
+                                <IconButton key="1"
+                                            onClick={this.handleExpandClick}
+                                            className={`BlockEditorView-expand ${this.state.expanded ? "expanded" : ""}`}
+                                            aria-expanded={this.state.expanded}
+                                            aria-label="Show more">
+                                    <ExpandMore color="primary"/>
+                                </IconButton>,
+                                <IconButton key="2" onClick={() => onEvent(BLOCK_EDITOR_EVENT.REMOVE)}>
+                                    <Clear color="primary"/>
+                                </IconButton>
+                            ]}/>
+                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        Content
+                    </CardContent>
+                </Collapse>
+            </Card>
+        );
+    }
+}
 
 BlockEditorView.propTypes = {
     block: PropTypes.object.isRequired,
