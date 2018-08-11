721355f696dac9dd7d00ebbce5a35d4ba59d718b
gbe <geoffrey.berard@lesfurets.com>
2018-08-11 22:59:35 +0200

    FormEngine    * Adding test for Utils Components

---
+++ test/component/utils/CtaTest.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {shallow} from "enzyme";
+import {EmailField} from "../../../src/component/fields/EmailField";
+import {initTest} from "../../test-utils";
+import {Cta, CTA_TYPE} from "../../../src/component/utils/Cta";
+import {BLOCK_EVENT} from "../../../src/component/wrapper/BlockWrapper";
+
+initTest();
+
+describe("FormEngine/Utils/Cta", () => {
+
+    describe("Type", () => {
+        it("should have type PRIMARY by default", () => {
+            // Given
+            let container = shallow(<Cta/>);
+
+            // Then
+            expect(container.find(`.${CTA_TYPE.PRIMARY}`).length).toBe(1);
+            expect(container.find(`.${CTA_TYPE.SECONDARY}`).length).toBe(0);
+        });
+
+        it("should have specified type", () => {
+            // Given
+            let container = shallow(<Cta type={CTA_TYPE.SECONDARY}/>);
+
+            // Then
+            expect(container.find(`.${CTA_TYPE.PRIMARY}`).length).toBe(0);
+            expect(container.find(`.${CTA_TYPE.SECONDARY}`).length).toBe(1);
+        });
+    });
+
+    describe("Click", () => {
+        it("should trigger onClick call", () => {
+            // Given
+            let onClick = jasmine.createSpy();
+            let container = shallow(<Cta onClick={onClick}/>);
+
+            // When
+            container.simulate("click");
+
+            // Then
+            expect(onClick).toHaveBeenCalled();
+        });
+    });
+
+});
