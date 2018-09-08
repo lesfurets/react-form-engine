4c347e255ad1a416062e833469a60e4cfd31a408
ozangunalp <ozangunalp@gmail.com>
2018-09-08 13:25:46 +0200

    Add ts compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,70 @@
 module.exports = (env, argv) => ({
-    entry: "./src/index.js",
-    output: {
-        path: __dirname + '/dist',
-        filename: 'index.js',
-        library: 'ReactContainerBoilerplate',
-        libraryTarget: 'umd',
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js',
+    library: 'ReactContainerBoilerplate',
+    libraryTarget: 'umd',
+  },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
     },
-    externals: [
-        {
-            react: {
-                root: 'React',
-                commonjs2: 'react',
-                commonjs: 'react',
-                amd: 'react',
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: {
+          loader: 'awesome-typescript-loader'
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      { enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              config: {path: __dirname, ctx: argv},
+              sourceMap: argv.mode !== "production",
             },
-        },
-        {
-            'react-dom': {
-                root: 'ReactDOM',
-                commonjs2: 'react-dom',
-                commonjs: 'react-dom',
-                amd: 'react-dom',
-            },
-        },
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.less$/,
-                use: ["style-loader",
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            config: {path: __dirname, ctx: argv},
-                            sourceMap: argv.mode !== "production",
-                        },
-                    },
-                    "less-loader"]
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
-            }
-        ]
-    }
+          },
+          "less-loader"]
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.less', '.css'],
+  },
+  devtool: 'source-map'
 });
