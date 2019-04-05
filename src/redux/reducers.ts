234dc3e611ef97b1f1ddf1efcc0f530090c7b864
ogu <Ozan.Gunalp@lesfurets.com>
2019-04-05 17:17:15 +0200

    reducers fix

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,7 +1,7 @@
 import {combineReducers, Reducer} from "redux";
 import {FieldContextState, FieldValueAction, SET_FIELD_VALUE} from "./constants";
 
-const fieldContext = (state: FieldContextState, action: FieldValueAction): Reducer<FieldContextState> => {
+const fieldContext : Reducer<FieldContextState> = (state: FieldContextState = {}, action: FieldValueAction) => {
     switch (action.type) {
         case SET_FIELD_VALUE:
             return {...state, [action.fieldId]: action.fieldValue};
