b36fbdd62da6c73713122b401d3b0b6e61a2aae7
ogu <Ozan.Gunalp@lesfurets.com>
2019-05-03 18:01:26 +0200

    Migrate FormEngineTest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     testMatch: ["**/test/**/*Test.ts", "**/test/**/*Test.tsx"],
+    moduleNameMapper: {
+        '\\.(scss|css|less)$': 'identity-obj-proxy'
+    },
     reporters: ["default", "jest-junit"],
     preset:"ts-jest"
 };
---
+++ test/component/FormEngineTest.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import ResponsiveContainer from "react-responsive-widget";
+
+import {shallow} from "enzyme";
+import {Provider} from "react-redux";
+import FormEngine from "../../src/component/FormEngine";
+import FormWrapper from "../../src/component/wrapper/FormWrapper";
+import {BlockViewProps} from "../../src/component/view/BlockView";
+import {Form} from "../../src/definition/FormModel";
+import {FieldTypes} from "../../src/definition/FieldTypes";
+import {FieldViewProps} from "../../src/component/view/FieldView";
+import {FormViewProps} from "../../src/component/view/FormView";
+import {EventCallBack} from "../../src/definition/event/EventMulticaster";
+import {TestUtils} from "../TestUtils";
+
+TestUtils.init();
+
+describe("FormEngine/Entry", () => {
+    let emptyCallback : EventCallBack = (event, source) => {};
+
+    let TestBlockView = (p: BlockViewProps) => (<div>{p.children}</div>);
+    let TestFieldView = (p: FieldViewProps) => (<div>{p}</div>);
+    let FormView = (p: FormViewProps) => (<div>{p}</div>);
+
+    let formModel: Form = {
+        id: "",
+        label: "",
+        blocks: [
+            {id: "block1", label: "", index: 0, fields: [{id: 'testChild1', type: FieldTypes.INPUT_TEXT}]},
+            {id: "block2", label: "", index: 0, fields: [{id: 'testChild2', type: FieldTypes.INPUT_TEXT}]}
+        ]
+    };
+
+    describe("Form", () => {
+        it("Should render Form", () => {
+            let container = shallow(<FormEngine
+                form={formModel}
+                onEvent={emptyCallback}
+                FormView={FormView}
+                BlockView={TestBlockView}
+                FieldView={TestFieldView}/>);
+            expect(container.find(FormWrapper).length).toBe(1);
+            expect(container.find(Provider).length).toBe(1);
+            expect(container.find(ResponsiveContainer).length).toBe(1);
+        });
+    });
+
+});
