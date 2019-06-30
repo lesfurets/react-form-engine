c4cc23b6ea1f9431565105d94283ee82a6476efe
gbe <geoffrey.berard@lesfurets.com>
2019-07-01 00:37:46 +0200

    FieldEditor 	* Allowing to remove StringPeoperties

--- example/app/view/editor/StringEditor.tsx
+++ example/app/view/editor/StringEditor.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import {PropertyEditor} from "../../definition/component/PropertyEditor";
 import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import Clear from "@material-ui/icons/Clear";
 
-export const StringEditor: PropertyEditor<string> = ({property, value, onChange}) => (
-    <TextField label={property.label}
-               value={value || ""}
-               className={`PropertyEditor ${property.key}`}
-               onChange={(event: React.ChangeEvent<HTMLInputElement>) => onChange(event.target.value)}
-               margin="normal"/>
+export const StringEditor: PropertyEditor<string> = ({property, value, onChange, onDelete}) => (
+    <div>
+        <TextField label={property.label}
+                   value={value || ""}
+                   className={`PropertyEditor ${property.key}`}
+                   onChange={(event: React.ChangeEvent<HTMLInputElement>) => onChange(event.target.value)}
+                   margin="normal"/>
+        <IconButton onClick={onDelete} ><Clear/></IconButton>
+    </div>
 );
