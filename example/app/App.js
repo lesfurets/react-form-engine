fa3da3af8e3a61aeb1271a59914f9359b832d267
gbe <geoffrey.berard@lesfurets.com>
2019-07-11 01:52:15 +0200

    Example    * Simplifying example

--- example/app/App.js
+++ example/app/App.js
@@ -1,158 +1,76 @@
 import React from "react";
-import "../styles/app.less";
 import FormEngine from "../../src/index";
 import {FieldTypes} from "../../src/definition/FieldTypes";
-import {ValidationUtils} from "../../src/definition/validation/ValidationUtils";
-import {VisibilityBuilder} from "../../src/dsl/visibility/VisibilityBuilder";
-import {ModelExtender} from "../../src/dsl/ModelExtender";
-import {ValidationBuilder} from "../../src/dsl/validation/ValidationBuilder";
-import {Predicates} from "../../src/dsl/predicate/builder/Predicates";
 
-const FIRST_NAME = {
-    id: "FIRST_NAME",
-    type: FieldTypes.INPUT_TEXT,
-    label: "First Name",
-    validationRule: ValidationBuilder.error("The first name is mandatory").when(Predicates.self.isNot.aString.startingWith("azer"))
-};
-
-const LAST_NAME = {
-    id: "LAST_NAME",
-    type: FieldTypes.INPUT_TEXT,
-    label: "Last Name",
-    visibilityRule: VisibilityBuilder.isNotVisible.when(Predicates.field(FIRST_NAME).isNot.defined())
-};
-
-const EMAIL = {
-    id: "EMAIL",
-    type: FieldTypes.INPUT_EMAIL,
-    label: "Email",
-};
-
-const PHONE = {
-    id: "PHONE",
-    type: FieldTypes.INPUT_TEXT,
-    label: "Phone",
-};
-
-const PASSWORD = {
-    id: "PASSWORD",
-    type: FieldTypes.INPUT_PASSWORD,
-    label: "Enter your password",
-};
-
-const PASSWORD_CONFIRMATION = {
-    id: "PASSWORD_CONFIRMATION",
-    type: FieldTypes.INPUT_PASSWORD,
-    label: "Confirm your password",
-    visibilityRule: VisibilityBuilder.isNotVisible.when(Predicates.field(FIRST_NAME).isNot.defined()),
-    getValidation(context) {
-        return ValidationUtils.isDefinedAndEqualTo(this, context, context[PASSWORD.id], "The passwords should be identical.");
-    }
-};
-
-const TEXT = {
-    id: "TEXT",
-    type: FieldTypes.INPUT_TEXT,
-    label: "Enter a text"
-};
-
-const NUMBER = {
-    id: "NUMBER",
-    type: FieldTypes.INPUT_INTEGER,
-    label: "Enter a number",
-};
-
-const AMOUNT = {
-    id: "AMOUNT",
-    type: FieldTypes.INPUT_DECIMAL,
-    label: "Enter an amount",
-    symbol: "€"
-};
-
-const TEXT_AREA = {
-    id: "TEXT_AREA",
-    type: FieldTypes.INPUT_TEXT_AREA,
-    label: "Enter a paragraph",
-};
-
-let SELECT_VALUES = [
+let VALUES = [
     {id: "value1", label: "Value 1"},
     {id: "value2", label: "Value 2"},
     {id: "value3", label: "Value 3"},
     {id: "value4", label: "Value 4"},
 ];
 
-const SELECT = {
-    id: "SELECT",
-    type: FieldTypes.INPUT_SELECT,
-    label: "Make your choice",
-    values: SELECT_VALUES
-};
-
-const RADIO = {
-    id: "RADIO",
-    type: FieldTypes.INPUT_RADIO,
-    label: "Make your choice",
-    values: SELECT_VALUES
-};
-
-const CHECKBOX = {
-    id: "CHECKBOX",
-    type: FieldTypes.INPUT_CHECKBOX,
-    label: "Select a radio",
-    values: SELECT_VALUES
-};
-
-const BLOCKS = {
-    ALL_FIELDS: {
-        id:"ALL_FIELDS",
-        label: "All Fields",
-        fields: [TEXT, PASSWORD , NUMBER , AMOUNT, TEXT_AREA, SELECT, RADIO, CHECKBOX]
-    },
-    IDENTITY: {
-        id:"IDENTITY",
-        label: "Personal information",
-        fields: [FIRST_NAME, LAST_NAME]
-    },
-    CONTACT: {
-        id:"CONTACT",
-        label: "Contact",
-        fields: [EMAIL, PHONE]
-    },
-    PASSWORD: {
-        id:"PASSWORD",
-        label: "Password",
-        fields: [PASSWORD, PASSWORD_CONFIRMATION],
-        ctaLabel: "Submit"
-    }
-};
-
 const FORM = {
-    id: "FORM",
-    blocks: Object.values(BLOCKS)
+    id: "EXAMPLE",
+    blocks: [
+        {
+            id: "ALL_FIELDS",
+            label: "All Fields",
+            fields: [
+                {
+                    id: "TEXT",
+                    type: FieldTypes.INPUT_TEXT,
+                    label: "Enter a text"
+                },
+                {
+                    id: "EMAIL",
+                    type: FieldTypes.INPUT_EMAIL,
+                    label: "Email",
+                },
+                {
+                    id: "PASSWORD",
+                    type: FieldTypes.INPUT_PASSWORD,
+                    label: "Enter your password",
+                },
+                {
+                    id: "NUMBER",
+                    type: FieldTypes.INPUT_INTEGER,
+                    label: "Enter a number",
+                },
+                {
+                    id: "AMOUNT",
+                    type: FieldTypes.INPUT_DECIMAL,
+                    label: "Enter an amount",
+                    symbol: "€"
+                },
+                {
+                    id: "TEXT_AREA",
+                    type: FieldTypes.INPUT_TEXT_AREA,
+                    label: "Enter a paragraph",
+                },
+                {
+                    id: "SELECT",
+                    type: FieldTypes.INPUT_SELECT,
+                    label: "Make your choice",
+                    values: VALUES
+                },
+                {
+                    id: "RADIO",
+                    type: FieldTypes.INPUT_RADIO,
+                    label: "Make your choice",
+                    values: VALUES
+                },
+                {
+                    id: "CHECKBOX",
+                    type: FieldTypes.INPUT_CHECKBOX,
+                    label: "Select a radio",
+                    values: VALUES
+                }
+            ]
+        }
+    ]
 };
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            model: ModelExtender.extendModel(FORM)
-        };
-        this.onEvent = this.onEvent.bind(this);
-    }
-
-    onEvent(event, element, details) {
-        console.log(event, element, details);
-    }
-
-    render() {
-        let {model} = this.state;
-        return (
-            <div className="FormExemple">
-                <FormEngine form={model} onEvent={this.onEvent}/>
-            </div>
-        );
-    }
-}
-
-export default App;
+export const App = () => (
+    <FormEngine form={FORM}
+                onEvent={(event, element, details) => console.log(event, element, details)}/>
+);
