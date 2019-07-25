255f1d651ee5b6a998276ff28ba83fd150b0b6ff
gbe <geoffrey.berard@lesfurets.com>
2019-07-25 10:54:57 +0200

    Example    Refactoring

---
+++ example/app/App.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import FormEngine from "../../src/index";
+import {FieldTypes} from "../../src/definition/FieldTypes";
+import {Field, FieldValue} from "../../src/definition/model/Field";
+import {Block} from "../../src/definition/model/Block";
+import {Form} from "../../src/definition/model/Form";
+
+let VALUES = [
+    {id: "value1", label: "Value 1"},
+    {id: "value2", label: "Value 2"},
+    {id: "value3", label: "Value 3"},
+    {id: "value4", label: "Value 4"},
+] as FieldValue[];
+
+const FormModel = {
+    id: "EXAMPLE",
+    blocks: [
+        {
+            id: "ALL_FIELDS",
+            label: "All Fields",
+            fields: [
+                {
+                    id: "TEXT",
+                    type: FieldTypes.INPUT_TEXT,
+                    label: "Enter a text",
+                },
+                {
+                    id: "EMAIL",
+                    type: FieldTypes.INPUT_EMAIL,
+                    label: "Email",
+                },
+                {
+                    id: "PASSWORD",
+                    type: FieldTypes.INPUT_PASSWORD,
+                    label: "Enter your password",
+                },
+                {
+                    id: "NUMBER",
+                    type: FieldTypes.INPUT_INTEGER,
+                    label: "Enter a number",
+                },
+                {
+                    id: "AMOUNT",
+                    type: FieldTypes.INPUT_DECIMAL,
+                    label: "Enter an amount",
+                    symbol: "€"
+                },
+                {
+                    id: "TEXT_AREA",
+                    type: FieldTypes.INPUT_TEXT_AREA,
+                    label: "Enter a paragraph",
+                },
+                {
+                    id: "SELECT",
+                    type: FieldTypes.INPUT_SELECT,
+                    label: "Make your choice",
+                    values: VALUES
+                },
+                {
+                    id: "RADIO",
+                    type: FieldTypes.INPUT_RADIO,
+                    label: "Make your choice",
+                    values: VALUES
+                },
+                {
+                    id: "CHECKBOX",
+                    type: FieldTypes.INPUT_CHECKBOX,
+                    label: "Select a radio",
+                    values: VALUES
+                },
+                {
+                    id: "DATE",
+                    type: FieldTypes.INPUT_DATE,
+                    label: "Enter a date"
+                },
+            ] as Field[]
+        }
+    ] as Block[]
+} as Form;
+
+export const App = () => (
+    <FormEngine form={FormModel}
+                onEvent={(event, element, details) => console.log(event, element, details)}/>
+);
